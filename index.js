8741f14ee31b7a18725483af30cb1d0d0b6bec0f
Dragos Nedelcu <dragosgn@gmail.com>
2019-03-16 13:56:09 +0100

    voice packages

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import runSampleVoice from "./runSampleVoice";
 
 const app = express();
 
 const port = 3000;
+
+const projectId = "tyrannosalesos";
+
+const answer = await runSampleVoice(projectId);
+
+console.log(answer, "answer");
 
 app.get("/", (req, res) => res.send("Helloo Dinooo"));
 
